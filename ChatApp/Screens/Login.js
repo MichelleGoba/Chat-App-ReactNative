a89f2debeb0a3d9bd7563b23ec6444ce4af9c19a
MichelleGoba <goba.michelleolwethu@gmail.com>
2023-11-08 15:57:08 +0200

    added title on the log in screen

--- ChatApp/Screens/Login.js
+++ ChatApp/Screens/Login.js
@@ -1,10 +1,18 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { KeyboardAvoidingView, StyleSheet, Text, View } from 'react-native'
+import React from 'react';
+import { Button } from 'react-native';
 
 const Login = () => {
   return (
-    <View>
-      <Text>Login</Text>
+    <View style={{flex:1, backgroundColor:"pinknp", padding:10, alignItems:"center"}}>
+      <KeyboardAvoidingView>
+        <View style={{marginTop:70, height:900, width:1300, backgroundColor:"grey",borderRadius:20}}>
+          <Text style={{color:"navy", fontSize:50, fontWeight:600, textAlign:"center", marginTop:20}}>Chat App</Text>
+          {/* <Button title="Log in" onPress={() => console.log("Button pressed")}></Button>
+          <Button>Sign in</Button> */}
+
+        </View>
+      </KeyboardAvoidingView>
     </View>
   )
 }
