2eeee3d29c7eada42c8e4c9fc6ae4d8abe127f48
MichelleGoba <goba.michelleolwethu@gmail.com>
2023-11-08 14:33:54 +0200

    installed json webtoken

---
+++ ChatApp/server/index.js
@@ -0,0 +1,16 @@
+// initialise dependencies
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const passport = require("passport")
+const LocalStrategy = require("passport-local").Strategy
+
+
+// instance for express application
+const app = express();
+const port = 8000;
+const cors = require("cors");
+
+app.use(bodyParser.urlencoded({ extended: false}));
+app.use(bodyParser.json());
+app.use(passport.initialize());
