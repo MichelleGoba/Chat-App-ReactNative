4326defbb5f577ab21b7529f6360eb2ac8605e49
MichelleGoba <goba.michelleolwethu@gmail.com>
2023-11-13 10:12:03 +0200

    added login styles

--- ChatApp/Screens/Login.js
+++ ChatApp/Screens/Login.js
@@ -1,22 +1,90 @@
-import { KeyboardAvoidingView, StyleSheet, Text, View } from 'react-native'
-import React from 'react';
-import { Button } from 'react-native';
+import {
+  KeyboardAvoidingView,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from "react-native";
+import React, { useState } from "react";
+import { Button } from "react-native";
 
 const Login = () => {
+  // to store the details values
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
   return (
-    <View style={{flex:1, backgroundColor:"pinknp", padding:10, alignItems:"center"}}>
+    <View style={styles.container}>
       <KeyboardAvoidingView>
-        <View style={{marginTop:70, height:900, width:1300, backgroundColor:"grey",borderRadius:20}}>
-          <Text style={{color:"navy", fontSize:50, fontWeight:600, textAlign:"center", marginTop:20}}>Chat App</Text>
+        <View
+          style={{
+            marginTop: 70,
+            height: 900,
+            width: 1300,
+            backgroundColor: "white",
+            borderRadius: 20,
+          }}
+        >
+          <Text style={styles.title}>Chat App</Text>
           {/* <Button title="Log in" onPress={() => console.log("Button pressed")}></Button>
           <Button>Sign in</Button> */}
 
+          <View style={styles.inputFields}>
+            <View>
+              <TextInput
+                value={username}
+                onChangeText={(text) => setUsername(text)}
+                style={styles.inputUsername}
+                placeholderTextColor={"grey"}
+                placeholder="Username"
+              />
+            </View>
+
+            <View>
+              <TextInput
+                value={password}
+                onChangeText={(text) => setPassword(text)}
+                style={styles.inputPassword}
+                placeholderTextColor={"grey"}
+                placeholder="Password"
+              />
+            </View>
+          </View>
         </View>
       </KeyboardAvoidingView>
     </View>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "pinknp",
+    padding: 10,
+    alignItems: "center",
+  },
+  title: {
+    color: "navy",
+    fontSize: 50,
+    fontWeight: 600,
+    textAlign: "center",
+    marginTop: 20,
+  },
+  inputFields: { marginTop: 50, padding: 50 },
+  inputUsername: {
+    borderColor: "gray",
+    borderWidth: 1,
+    borderRadius: 10,
+    width: 300,
+    height: 50,
+  },
+  inputPassword: {
+    marginTop: 20,
+    borderColor: "gray",
+    borderWidth: 1,
+    borderRadius: 10,
+    width: 300,
+    height: 50,
+  },
+});
